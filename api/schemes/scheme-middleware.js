03b3a59d7fdbe3ade7e496538cc39bfb0119bbc3
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-08 17:40:38 -0400

    "MVP"

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,55 +1,37 @@
-const Scheme = require("./scheme-model");
-//const db = require("../../data/schemes.db3");
-/*
-  If `scheme_id` does not exist in the database:
+const db = require("../../data/db-config");
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = async (req, res, next) => {
   try{
-    const scheme = await Scheme.findById(req.params.id)
-    if(!scheme){ next({status:404, message: `scheme with scheme_id ${req.params.id}not found`}) }
+    const existingScheme = await db("schemes")
+        .where("scheme_id", req.params.scheme_id)
+        .first()
+    if(!existingScheme){ next({status:404, message: `scheme with scheme_id ${req.params.scheme_id} not found`}) }
+    else{next()}
   }
   catch(err){next(err)}
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
 
-  status 400
-  {
-    "message": "invalid scheme_name"
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body
+  if ( scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+    next({ status: 400, message: 'invalid scheme_name' })
   }
-*/
-const validateScheme = async (req, res, next) => {
-  const scheme_name = req.body;
-  if (scheme_name === undefined || typeof scheme_name !== "string" || scheme_name === ""){
-    next({status: 400, message: `invalid scheme_name`})
-  }
-  else{req.body.scheme = scheme_name.trim()}
+  else { next() }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
   const {instructions, step_number} =  req.body;
   if (instructions === undefined ||
       typeof instructions !== "string" ||
-      instructions === "" ||
-      step_number === isNaN() ||
-      step_number < 1){
-    next({status: 400, message: "invalid step"})
+      !instructions.trim() ||
+      typeof step_number !== "number" ||
+      step_number < 1) {
+    const error = {status: 400, message: "invalid step"}
+    next(error)
   }
+  else{next()}
 }
+
 
 module.exports = { checkSchemeId, validateScheme, validateStep,}
