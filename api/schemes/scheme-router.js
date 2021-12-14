ce6669cd09caab4053770217911abd3e9f69e0f4
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2021-12-14 09:05:10 -0500

    Update scheme-router.js
    deleted comments

--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -5,24 +5,7 @@ const Schemes = require('./scheme-model.js')
 
 const router = express.Router()
 
-/**
-  [GET] /api/schemes
 
-  response:
-  [
-    {
-      "scheme_id": 1,
-      "scheme_name": "World Domination",
-      "number_of_steps": 3
-    },
-    {
-      "scheme_id": 2,
-      "scheme_name": "Get Rich Quick",
-      "number_of_steps": 2
-    },
-    // etc
-  ]
- */
 router.get('/', (req, res, next) => {
   Schemes.find()
     .then(schemes => {
@@ -31,27 +14,7 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-/*
-  [GET] /api/schemes/2
 
-  response:
-  {
-    "scheme_id": 2,
-    "scheme_name": "Get Rich Quick",
-    "steps": [
-      {
-          "step_id": 5,
-          "step_number": 1,
-          "instructions": "collect all the sheep in Scotland"
-      },
-      {
-          "step_id": 4,
-          "step_number": 2,
-          "instructions": "profit"
-      }
-    ]
-  }
-*/
 router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
   const { scheme_id } = req.params
 
@@ -62,25 +25,7 @@ router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
     .catch(next)
 })
 
-/*
-  [GET] /api/schemes/2/steps
 
-  response:
-  [
-    {
-      "step_id": 5,
-      "step_number": 1,
-      "instructions": "collect all the sheep in Scotland",
-      "scheme_name": "Get Rich Quick"
-    },
-    {
-      "step_id": 4,
-      "step_number": 2,
-      "instructions": "profit",
-      "scheme_name": "Get Rich Quick"
-    }
-  ]
-*/
 router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
   const { scheme_id } = req.params
 
@@ -91,15 +36,7 @@ router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
     .catch(next)
 })
 
-/*
-  [POST] /api/schemes { "scheme_name": "Take Ovah" }
 
-  response:
-  {
-    "scheme_id": 8,
-    "scheme_name": "Take Ovah"
-  }
-*/
 router.post('/', validateScheme, (req, res, next) => {
   const scheme = req.body
 
@@ -110,25 +47,7 @@ router.post('/', validateScheme, (req, res, next) => {
     .catch(next)
 })
 
-/*
-  [POST] /api/schemes/5/steps { "instructions": "and yet more questing", "step_number": 2 }
 
-  response:
-  [
-    {
-      "step_id": 12,
-      "step_number": 1,
-      "instructions": "quest and quest some more",
-      "scheme_name": "Find the Holy Grail"
-    },
-    {
-      "step_id": 17,
-      "step_number": 2,
-      "instructions": "and yet more questing",
-      "scheme_name": "Find the Holy Grail"
-    }
-  ]
-*/
 router.post('/:scheme_id/steps', checkSchemeId, validateStep, (req, res, next) => {
   const step = req.body
   const { scheme_id } = req.params
